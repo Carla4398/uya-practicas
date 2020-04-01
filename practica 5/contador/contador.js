cc75b46635e4bc5165ae76bbd38fa05f0b71c1be
Carla <Caroto.4398@gmail.com>
2020-04-01 22:06:05 +0100

    Update contador.js

--- practica 5/contador/contador.js
+++ practica 5/contador/contador.js
@@ -1,1 +1,18 @@
+function getAllParaElems()
+{
+  var allp = document.getElementsByTagName("p");
+
+  var nump = allp.length;
+  
+  var alle = document.getElementsByTagName("a");
+
+  var nume = alle.length;
+  
+  var allh = document.getElementsByTagName("head");
+
+  var numh = allh.length;
+
+  alert("Hay "+ nump + " elementos <p>, " + nume + " elementos <a> y "+ numh + " elementos <head> en este documento");
+  
+}
 
